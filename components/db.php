b419c8233e2b4ecd0de63ace9314a68c6f5a24a0
Jellinek <jellinekbraus@gmail.com>
2023-03-19 18:18:09 +0500

    some changes

--- components/db.php
+++ components/db.php
@@ -7,3 +7,29 @@ function pdo() {
     $host = 'localhost';
     return new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 }
+
+function userReg($login,$pass,$name,$photo) {
+    $pdo = pdo();
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    $query = "INSERT INTO users (login, pass, name, photo) VALUES (?,?,?,?)";
+    $driver = $pdo->prepare($query);
+    $result = $driver->execute([$login,$pass,$name,$photo]);
+    
+    return $result;
+}
+
+
+function userSign($login,$pass) {
+    $pdo = pdo();
+    $query = "SELECT * FROM users WHERE login=?";
+    $driver = $pdo->prepare($query);
+    $result = $driver-> execute([$login]);
+    $user = $driver->fetch(PDO::FETCH_ASSOC);
+    if($user['login'] == $login && password_verify($pass, $user['pass'])) {
+       $_SESSION['login'] = $user['login'];
+       $_SESSION['photo'] = $user['photo'];
+    }else {
+        return false;
+    }
+}
+
