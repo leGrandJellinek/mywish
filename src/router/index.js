0f54c11256c16250ecf2bb634ff97ce158f0f4e5
Jěllinék <98526902+leGrandJellinek@users.noreply.github.com>
2023-11-03 00:05:37 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import InDevelope from "@/views/InDevelope";
 
@@ -18,7 +18,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
